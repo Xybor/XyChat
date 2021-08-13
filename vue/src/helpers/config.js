30115d073693647e2bdfadd57a07d34c2b327bdb
h40huynh <h40huynh@gmail.com>
2021-08-13 15:17:04 +0700

    Fix protocol

--- vue/src/helpers/config.js
+++ vue/src/helpers/config.js
@@ -1,11 +1,13 @@
 var currentHost = window.location.host;
-var schema = window.location.protocol;
+var apiSchema = "https:";
+var wsSchema = "ws:";
 
 if (process.env.NODE_ENV == "development") {
   currentHost = `${window.location.hostname}:1999`;
+  apiSchema = "http:";
 }
 
 export const configs = {
-  apiUrl: `${schema}//${currentHost}/api/v1`,
-  wsUrl: `ws://${currentHost}/ws/v1`,
+  apiUrl: `${apiSchema}//${currentHost}/api/v1`,
+  wsUrl: `${wsSchema}://${currentHost}/ws/v1`,
 };
